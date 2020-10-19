a629af29b4d40a5cb9c9cc25bea8d373bbe8f6ac
Ubertalli  Pierrot <M1905139@middlesbro.ac.uk>
2020-10-19 09:36:44 +0100

    first commit

---
+++ src/classone/ClassOne.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classone;
+
+/**
+ *
+ * @author M1905139
+ */
+public class ClassOne {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num1;
+        int num2;
+        int largest ;
+        num1=+;
+        num2=4;
+        answer= num1=num2;
+        System.out.println("the total of the two numbers is"+ answer);
+      
+        
+        
+        
+      
+// TODO code application logic here
+    }
+    
+}
